16869a4fabbdc4a66c2e5af79c158e8549a33b79
Md Dablu Hossain <dabluhossain@gmail.com>
2023-07-03 08:12:22 +0600

    cimmited

---
+++ components/Products.js
@@ -0,0 +1,16 @@
+var products =[
+    {
+        id:1,
+        name:"Laptop",
+        description:"HP Laptop",
+        price:500,
+        imageUrl:"assets/images/products/product1.jpg"
+    },
+    {
+        id:2,
+        name:"Laptop-1",
+        description:"Dell Laptop",
+        price:600,
+        imageUrl:"assets/images/products/product1.jpg"
+    }
+];
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    // "./pages/**/*.{html,js}",
+    // "./src/**/*.{html,js}",
+    // "./components/**/*.{html,js}",
+  ],
+  theme: {
+    // extend: {},
+    container:{
+      center:true,
+    }
+  },
+  plugins: [],
+}
+
